53003716164b2b502a8aa9309fdd7234a441bf9e
carr <1129579561@qqq.com>
2019-07-22 21:54:08 +0800

    add work

--- lodash/carforme-lodash.js
+++ lodash/carforme-lodash.js
@@ -8,16 +8,16 @@ var carforme = {
         }
         return array
     },
-    /** flattenDeep: function(array) {
+    flattenDeep: function(array) {
         return array.reduce((result, item) => {
-            if (array.isArray(item)) {
-                result.push(...carforme.flattenDeep(item))
+            if (Array.isArray(item)) {
+                result.push(...flattenDeep(item))
             } else {
                 result.push(item)
             }
             return result
         }, [])
-    },**/
+    },
     isArray: function(array) {
         return Object.prototype.toString.call(array) === '[object Array]'
     },
@@ -78,9 +78,66 @@ var carforme = {
         }
         return sum + String(array[i])
 
-    }
-
-
+    },
+    head: function(array) {
+        if (array[0]) {
+            return array[0]
+        }
+    },
+    indexOf: function(array, value, fromIndex = 0) {
+        for (var i = fromIndex; i < array.length; i++) {
+            if (array[i] == value) {
+                return i
+            }
+        }
+        return -1
+    },
+    lastIndexOf: function(array, value, fromIndex = array.length - 1) {
+        for (var i = fromIndex; i >= 0; i--) {
+            if (array[i] == value) {
+                return i
+            }
+        }
+        return -1
+    },
+    initial: function(array) {
+        return array.slice(0, array.length - 1)
+    },
+    pull: function(array, ...values) {
+        for (var i = 1; i < arguments.length; i++) {
+            array.forEach((element, index) => {
+                if (element == arguments[i]) {
+                    array.splice(index, 1)
+                }
+            })
+        }
+        return array
+    },
+    reverse: function(array) {
+        var result = []
+        array.forEach((element, index) => {
+            result[array.length - 1 - index] = element
+        })
+        return result
+    },
+    sortedIndex: function(array, value) {
+        if (value <= array[0] || value >= array[array.length - 1]) {
+            return value <= array[0] ? 0 : array.length
+        }
+        var start = 0,
+            last = array.length - 1;
+        while (last > start) {
+            mid = Math.floor((start + last) / 2)
+            if (array[mid] < value && value <= array[mid + 1]) {
+                return mid + 1
+            }
+            if (array[mid] < value) {
+                start = mid
+            } else {
+                last = mid
+            }
+        }
+    },
 
 
 }
